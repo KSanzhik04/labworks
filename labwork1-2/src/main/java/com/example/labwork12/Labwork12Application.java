34cc41a90eb915aa390a59352f3dad7c30dea75d
KSanzhik04 <139846265+KSanzhik04@users.noreply.github.com>
2024-11-23 22:06:58 +0500

    Add files via upload

---
+++ labwork1-2/src/main/java/com/example/labwork12/Labwork12Application.java
@@ -0,0 +1,13 @@
+package com.example.labwork12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labwork12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Labwork12Application.class, args);
+    }
+
+}
